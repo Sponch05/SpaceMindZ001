3208460c67e3f196d45724e67d43fafc7ac2a125
sirMothupii <iamkeenan94@gmail.com>
2017-09-30 17:13:20 +0200

    Tabbed view added

--- app/src/main/java/com/spacemindz/www/spacemindz001/Register.java
+++ app/src/main/java/com/spacemindz/www/spacemindz001/Register.java
@@ -1,5 +1,6 @@
 package com.spacemindz.www.spacemindz001;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,10 @@ public class Register extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+    }
+    //Submit button to take user to Login screen
+    {
+        Intent intent1 = new Intent(this, MainActivity.class);
+        startActivity(intent1);
     }
 }
--- app/src/main/java/com/spacemindz/www/spacemindz001/Services.java
+++ app/src/main/java/com/spacemindz/www/spacemindz001/Services.java
@@ -3,11 +3,13 @@ package com.spacemindz.www.spacemindz001;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class Sales extends AppCompatActivity {
+public class Services extends AppCompatActivity
+{
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sales);
+        setContentView(R.layout.activity_services);
     }
 }
