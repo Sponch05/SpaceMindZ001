678a923106cffd6ed993ce80b0d6e46e820b168c
sirMothupii <iamkeenan94@gmail.com>
2017-10-01 11:08:21 +0200

    OnClick listener intents added to Screens

--- app/src/main/java/com/spacemindz/www/spacemindz001/Content.java
+++ app/src/main/java/com/spacemindz/www/spacemindz001/Content.java
@@ -1,7 +1,9 @@
 package com.spacemindz.www.spacemindz001;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class Content extends AppCompatActivity
 {
@@ -11,5 +13,25 @@ public class Content extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_content);
+    }
+
+    public void onClickKotas(View view)
+      //On click to the kota category class
+    {
+        Intent intent1 = new Intent(this, KotasOnline.class);
+        startActivity(intent1);
+    }
+
+    public void onClickCreatives(View view)
+    //On click to the clothing category class
+    {
+        Intent intent1 = new Intent(this, KasiCreatives.class);
+        startActivity(intent1);
+    }
+    public void onClickFruitAndVeg(View view)
+    //On click to the vendors category class
+    {
+        Intent intent1 = new Intent(this, FruitAndVeg.class);
+        startActivity(intent1);
     }
 }
