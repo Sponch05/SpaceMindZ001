1dd174a7dfb5d44471d01a1af2985dfc18d73224
sirMothupii <iamkeenan94@gmail.com>
2017-10-01 09:10:02 +0200

    apdated content

---
+++ app/src/main/java/com/spacemindz/www/spacemindz001/TheCreatives.java
@@ -0,0 +1,15 @@
+package com.spacemindz.www.spacemindz001;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TheCreatives extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_the_creatives);
+    }
+}
